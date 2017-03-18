fc191a76ec11e0db6c166d90984b7104cf4d77a0
Philip Davis <learningd3js@gmail.com>
2017-03-18 14:25:36 -0700

    analytics set and pageview

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import ReactGA from 'react-ga';
 
-var ReactGA = require('react-ga');
 ReactGA.initialize('UA-51640724-1');
+ReactGA.set({ page: window.location.pathname });
+ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(
   <App />,
