2783d51f7dc547447bfb36679a1e16c630d68acf
Philip Davis <learningd3js@gmail.com>
2017-03-17 18:17:39 -0700

    Removed titles

--- src/Gallery.js
+++ src/Gallery.js
@@ -8,20 +8,15 @@ export default class Gallery extends Component {
     return (
       <div className="max-width-3">
         <GalleryItem  src="img/static-circles.gif" />
-        <div className="mb4">Pupil</div>
 
         <GalleryItem  src="https://learningd3.com/blog/assets/img/posts/generative-art/ring-of-bars.gif" />
-        <div className="mb4">Ring of Bars</div>
 
         <GalleryItem  src="https://learningd3.com/blog/assets/img/posts/generative-art/circle-tunnel.gif" />
-        <div className="mb4">Circle Tunnel</div>
 
         <GalleryItem  src="https://learningd3.com/blog/assets/img/posts/generative-art/static.gif" />
-        <div className="mb4">Static Lines</div>
 
         <GalleryItem  src="https://learningd3.com/blog/assets/img/posts/generative-art/moire.jpg" />
-        <div className="mb4">Moire</div>
-
+        
       </div>
     );
   }
